1bbcc0d900f313db194d867813bd2b66b410cb34
Moise Rebatta <moiserebatta@gmail.com>
2021-10-28 09:05:51 -0300

    added force links

--- configure.sh
+++ configure.sh
@@ -6,11 +6,11 @@ fi
 
 DOTFILES_PATH=$HOME/.dotfiles
 
-ln -s $DOTFILES_PATH/.config/terminator $HOME/.config/terminator
-ln -s $DOTFILES_PATH/.zshrc $HOME/.zshrc
-ln -s $DOTFILES_PATH/.p10k.zsh $HOME/.p10k.zsh
-ln -s $DOTFILES_PATH/.vimrc $HOME/.vimrc
-ln -s $DOTFILES_PATH/.config/bat $HOME/.config/bat
-ln -s $DOTFILES_PATH/.config/nvim $HOME/.config/nvim
-ln -s $DOTFILES_PATH/.config/Code/User/keybindings.json $HOME/.config/Code/User/keybindings.json
-ln -s $DOTFILES_PATH/.config/Code/User/settings.json $HOME/.config/Code/User/settings.json
+ln -sf $DOTFILES_PATH/.config/terminator $HOME/.config/terminator
+ln -sf $DOTFILES_PATH/.zshrc $HOME/.zshrc
+ln -sf $DOTFILES_PATH/.p10k.zsh $HOME/.p10k.zsh
+ln -sf $DOTFILES_PATH/.vimrc $HOME/.vimrc
+ln -sf $DOTFILES_PATH/.config/bat $HOME/.config/bat
+ln -sf $DOTFILES_PATH/.config/nvim $HOME/.config/nvim
+ln -sf $DOTFILES_PATH/.config/Code/User/keybindings.json $HOME/.config/Code/User/keybindings.json
+ln -sf $DOTFILES_PATH/.config/Code/User/settings.json $HOME/.config/Code/User/settings.json
