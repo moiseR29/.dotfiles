83fbc7ef5c6563431f5aa77c5dcc5788670e4f68
Moises Rebatta <moiserebatta@gmail.com>
2023-06-10 01:31:49 -0300

    update configure script

--- configure.sh
+++ configure.sh
@@ -1,4 +1,3 @@
-# Verify exists folder
 if [ ! -d $HOME/.dotfiles ]; then
   echo "Please clone repo, REPO and it was clone in .dotfiles folder on root"
   exit 1
@@ -6,11 +5,15 @@ fi
 
 DOTFILES_PATH=$HOME/.dotfiles
 
-ln -s $DOTFILES_PATH/.config/terminator $HOME/.config/terminator
-ln -s $DOTFILES_PATH/.zshrc $HOME/.zshrc
-ln -s $DOTFILES_PATH/.p10k.zsh $HOME/.p10k.zsh
-ln -s $DOTFILES_PATH/.vimrc $HOME/.vimrc
-ln -s $DOTFILES_PATH/.config/bat $HOME/.config/bat
-ln -s $DOTFILES_PATH/.config/nvim $HOME/.config/nvim
-ln -s $DOTFILES_PATH/.config/Code/User/keybindings.json $HOME/.config/Code/User/keybindings.json
-ln -s $DOTFILES_PATH/.config/Code/User/settings.json $HOME/.config/Code/User/settings.json
+ln -sf $DOTFILES_PATH/.config/terminator $HOME/.config/terminator
+ln -sf $DOTFILES_PATH/.zshrc $HOME/.zshrc
+ln -sf $DOTFILES_PATH/.p10k.zsh $HOME/.p10k.zsh
+ln -sf $DOTFILES_PATH/.vimrc $HOME/.vimrc
+ln -sf $DOTFILES_PATH/.config/alacritty $HOME/.config/alacritty
+ln -sf $DOTFILES_PATH/.config/bat $HOME/.config/bat
+ln -sf $DOTFILES_PATH/.config/Code/User/keybindings.json $HOME/.config/Code/User/keybindings.json
+ln -sf $DOTFILES_PATH/.config/Code/User/settings.json $HOME/.config/Code/User/settings.json
+ln -sf $DOTFILES_PATH/.config/dunst $HOME/.config/dunst
+ln -sf $DOTFILES_PATH/.config/nvim $HOME/.config/nvim
+ln -sf $DOTFILES_PATH/.config/picom.conf $HOME/.config/picom/picom.conf
+ln -sf $DOTFILES_PATH/.config/qtile $HOME/.config/qtile
